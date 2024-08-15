f7226d93a3f0147e2f70f6efff297cea66fbe344
Karina <contacto.karinarojas@gmail.com>
2024-08-15 15:34:43 -0400

    Actualizacion - Hito 4 - Se agrega autenticacion de usuario, decorador login_required, {{user.get_username}}., boton cerrar sesión, 3 usuarios creados desde admin

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7rnu21s^g7ud&#qf&6td%er+e5mqju0h$18n+b_$)k55be7w(q'
+SECRET_KEY = 'django-insecure-ag=d)!%y^nq%=4ip1zd@7pvfx*#nv&4lsa0s2sti($iy9oqj#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/bienvenido'
+LOGOUT_REDIRECT_URL = '/'
